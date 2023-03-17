32cbac12d9f6066cc3bb24a28be87476ce52132f
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-03-17 19:00:22 +0330

    fix: proxy-server issue by utilizing express-proxy-server

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "url";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dropmail.me/api/graphql/mylongtoken',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    }
-  },
-  resolve: {
-    alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-    ]
-  },
   plugins: [react()],
 });
